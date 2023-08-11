60218f5558d14a3391a4ec764b2c8301d9adbb76
temevh <temehama@gmail.com>
2023-08-11 10:52:38 +0300

    added chart

--- expensesTracker/expensetracker/lib/widgets/expenses.dart
+++ expensesTracker/expensetracker/lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expensetracker/widgets/chart/chart.dart';
 import 'package:expensetracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expensetracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -88,7 +89,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text("The chart"),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
